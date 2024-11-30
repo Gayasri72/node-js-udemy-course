327626261f498011fba4c2a4e1cfcbbcb81d596d
Gayasri72 <gayasripethum1@gmail.com>
2024-11-30 12:35:38 +0530

    complete the node farm project

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,11 +1,13 @@
 // reading data and writing data using fs module
 
 // we can use import statement without install package.json with help of .mjs file extension
-// const fs = require("fs");
+const fs = require("fs");
 // import fs from 'fs';
 // include http module
 const http = require("http");
-const url= require("url");
+const url = require("url");
+//our module to replace the template
+const replaceTemplate=require("./modules/replaceTemplate");
 // import http from 'http';
 
 // const hello = "Hello world!";
@@ -50,27 +52,57 @@ const url= require("url");
 
 // console.log("hello!");
 //----------------------------------------------------------------------------------------------
-//creating server
-const server = http.createServer((req, res) => {
 
-    const pathName=req.url;
-    if(pathName==="/" || pathName==="/overview"){
-        res.end("This is the overview");
-    }
-    else if(pathName==="/product"){
-        res.end("This is the product");
-    }
-    else{
-        res.writeHead(404,{
-            "Content-type":"text/html",
-            "my-own-header":"hello-world"
-        });
-        res.end("<h1>WTF! No page Buddy😒</h1>");
-    }
+
+const tempOverview=fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8", )
+const tempCard=fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8", )
+const tempProduct=fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8", )
+
+
+
+const data=fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8", )
+    
+
+const dataObj = JSON.parse(data);
+ 
+//creating server
+
+const server = http.createServer((req, res) => {
+    const {query,pathname}=url.parse(req.url,true)
+ 
+
+  //OVERVIEW PAGE
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml=dataObj.map(el=>replaceTemplate(tempCard,el)).join('')
+    const output=tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
+    res.end(output);
+
+  //PRODUCT PAGE  
+  } else if (pathname === "/product") {
+    const product=dataObj[query.id]
+    res.writeHead(200, { "Content-type": "text/html" });
+    const output=replaceTemplate(tempProduct,product)
+    res.end(output);
+
+
+    //API PAGE
+  } else if (pathname === "/api") {
+    
+      res.writeHead(200, { "Content-type": "application/json" });
+      res.end(data);
+    
+
+    //NOT FOUND PAGE
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>WTF! No page Buddy😒</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to request on port 8000");
 });
-
-
